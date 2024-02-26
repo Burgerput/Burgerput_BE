24ea24dcf46a311308adb756e815616631f86628
YellTa <>
2024-02-26 22:08:08 +0900

    Delete front end files add const file setup to git - delete Const.java from gitignore

---
+++ src/main/java/burgerput/project/zenput/Const.java
@@ -0,0 +1,29 @@
+package burgerput.project.zenput;
+public class Const {
+
+    //chrome driver location
+//    local TEST driver location
+//    public static final String DRIVERLOCATION = "C:/Users/bbubb/Desktop/Burgerput/chromeDriver/chromedriver.exe";
+//    public static final String MACHINEURL="C://Users/bbubb/Desktop/Burgerput/burgerput-main/burgerputClone4/BURGERPUT/src/main/resources/static/zenput/zenput_machine/Zenput.html";
+////    public static final String FOODURL="C://Users/bbubb/Desktop/Burgerput/burgerput-main/burgerputClone4/BURGERPUT/src/main/resources/static/zenput/zenput_food/Zenput.html";
+//
+//    public static final String FOODURL="C://Users/bbubb/Desktop/Burgerput/burgerput-main/burgerputClone4/BURGERPUT/src/main/resources/static/zenput/zenput_food/zenput.mhtml";
+//
+
+//    not local link
+    public static final String DRIVERLOCATION = "file://"+"/home/ubuntu/burgerput/ref/chromedriver";
+
+    public static final String MACHINEURL="file://"+"/home/ubuntu/burgerput/ref/zenput/zenput_machine/Zenput.html";
+
+    public static final String FOODURL="file://"+"/home/ubuntu/burgerput/ref/zenput/zenput_food/Zenput.html";
+
+    //real zenput page
+    public static final String zenputPageStart="https://www.zenput.com/mobile/";
+
+    //request link - burgerput domain
+//    public static final String BURGERPUTSITE="burgerput.co.kr";
+    public static final String BURGERPUTSITE="https://localhost:8080/";
+
+
+}
+
